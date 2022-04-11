08949ee4792e2e5f2c397efc11ed3c3296776a43
phani003 <vangaveetiphani@gmail.com>
2022-04-11 06:56:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
-port=$PORT\n\
+headless = true\n\
 enableCORS=false\n\
-headless=true\n\
-\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
